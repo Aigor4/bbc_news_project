cec7b1cda40e1bdff3ffbb3fde1ed5ecf95e13e8
Aigor4 <144654566+Aigor4@users.noreply.github.com>
2026-08-26 23:18:25 +0200

    Update etl.py

--- src/etl.py
+++ src/etl.py
@@ -17,11 +17,12 @@ def transform(text):
 def load(path):
     df = extract(path)
     df['data'] = df['data'].apply(transform)
-    df['labels'] = pd.factorize(df['labels'])[0]
+    codes,names = pd.factorize(df['labels'])
+    df['labels'] = codes
     X = df['data']
     y = df['labels']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
-    return X_train, X_test, y_train, y_test
+    return X_train, X_test, y_train, y_test, names
 
 
 
